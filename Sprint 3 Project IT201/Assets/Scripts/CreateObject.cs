850b90675423f84f8a470d4f20c33ded79f0bb11
LM <ldm29@njit.edu>
2023-12-12 17:22:39 -0500

    Added basic navmesh AI and arrow instantiation

--- Sprint 3 Project IT201/Assets/Scripts/CreateObject.cs
+++ Sprint 3 Project IT201/Assets/Scripts/CreateObject.cs
@@ -2,32 +2,37 @@ using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
 
-public class CreateObject : MonoBehaviour {
+public class CreateObject : MonoBehaviour
+{
     public float distance = 0f;
-    float posX = -1f;
-    float posY = -1f;
-    float posZ = -1f;
-    Rigidbody rb;
-  //  public GameObject cylinder;
+    public GameObject arrow1;
+    public AudioSource arrowNoise;
+    float lastArrowTime = .5f;
 
-    // https://docs.unity3d.com/560/Documentation/ScriptReference/UI.Slider-value.html
-    // https://pastebin.com/5fhPHtYa
-    // altered so it constantly updates the object which displays the current color,
-    // only places objects if within a certain limit of the mousePosition,
-    // changes shape depending on dropdown chosen,
-    // and changes colors depending on sliders
-    void Update() {
-            if (Input.GetMouseButton(0) || Input.GetMouseButton(1)) {
-                    posX = -1f;
-                    posY = -1f;
-                    posZ = -1f;
-                    Vector3 clickPosition = new Vector3(posX, posY, posZ);
-                    clickPosition = Camera.main.ScreenToWorldPoint(Input.mousePosition + new Vector3(0f, 0f, distance));
-                    GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
-                    cylinder.tag = "Primitive";
-                // rb.velocity = new Vector3(1, rb.velocity.y, rb.velocity.z);
-                    cylinder.transform.position = clickPosition;
-                }
-            }
-}      
+    void Update()
+    {
+        if ((Time.time - lastArrowTime) >= .5f && (Input.GetMouseButton(0) || Input.GetMouseButton(1)))
+        {
+            arrowNoise.Play();
+            
+            Vector3 screenCenter = new Vector3(Screen.width / 2f, Screen.height / 2f, distance);
 
+            Vector3 clickPosition = Camera.main.ScreenToWorldPoint(screenCenter);
+
+            InstantiateArrow(clickPosition);
+            lastArrowTime = Time.time;
+        }
+    }
+
+    void InstantiateArrow(Vector3 position)
+    {
+        // Instantiate arrow with rotation based on the camera's forward direction
+        GameObject arrow = Instantiate(arrow1, position, Camera.main.transform.rotation);
+        Rigidbody arrowRb = arrow.GetComponent<Rigidbody>();
+
+        arrowRb.velocity = Camera.main.transform.forward * 40f;
+        arrowRb.useGravity = true;
+
+        Destroy(arrow, 5);
+    }
+}
