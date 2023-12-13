80447ad31de6e30bd8d160292d29b36f9ed08ee3
LM <ldm29@njit.edu>
2023-12-13 01:05:05 -0500

    Added some enemies

---
+++ Sprint 3 Project IT201/Assets/Scripts/EnemyHealth/EC3_MinorHealth.cs
@@ -0,0 +1,32 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EC3_MinorHealth : MonoBehaviour
+{
+    public int Health = 100;
+    public int damage = 20;
+    public GameObject Enemy;
+    public AudioSource EnemyHit;
+    public TextMeshProUGUI Currency;
+    public int EnemyVal;
+    int curr;
+
+    void Update() {
+        if (Health <= 0) {
+            Currency.text = (curr+EnemyVal).ToString();
+            Destroy(Enemy);
+        }
+        curr = int.Parse(Currency.text);
+    }
+
+    void OnTriggerEnter (Collider other) {
+        if (other.gameObject.tag == "Arrow") {
+            Health -= damage;
+            EnemyHit.Play();
+        }
+    }
+}
